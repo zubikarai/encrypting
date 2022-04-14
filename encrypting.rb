6286494c00d82a9f3d99d8ff28807f638901facc
zubikarai <7schets7@gmail.com>
2022-04-15 01:16:38 +0500

    Added new branch

--- encrypting.rb
+++ encrypting.rb
@@ -6,10 +6,11 @@ user_word = STDIN.gets.chomp
 puts "Каким способом зашифровать?"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 user_method = STDIN.gets.to_i
 
-until user_method.between?(1, 2)
-  puts "Выберите 1 или 2"
+until user_method.between?(1, 3)
+  puts "Выберите 1, 2 или 3"
   user_method = STDIN.gets.to_i
 end
 
@@ -18,4 +19,5 @@ puts "Вот и шифровка:"
 case user_method
 when 1 then puts Digest::MD5.hexdigest user_word
 when 2 then puts Digest::SHA1.hexdigest user_word
+when 3 then puts Digest::SHA2.hexdigest user_word
 end
